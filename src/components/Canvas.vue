931e987ff71fb11ba6665b908d3f3b8dbb10fc5d
HFourmy <nice_fourmy@hotmail.com>
2021-03-08 11:21:53 +0100

    Added undo to canvas

--- src/components/Canvas.vue
+++ src/components/Canvas.vue
@@ -11,6 +11,9 @@
 		>
 			{{ id }} Désolé, votre navigateur ne prend pas en charge &lt;canvas&gt;.
 		</canvas>
+		<button @click="loadPreviousCanvasState">
+			Undo
+		</button>
 	</div>
 </template>
 
@@ -24,11 +27,18 @@ export default Vue.extend({
 	},
 	data(): {
 		canvasContext: CanvasRenderingContext2D | null;
+		canvasStates: any[];
 		isMouseDown: boolean;
 		x: number;
 		y: number;
 	} {
-		return { canvasContext: null, isMouseDown: false, x: 0, y: 0 };
+		return {
+			canvasContext: null,
+			isMouseDown: false,
+			x: 0,
+			y: 0,
+			canvasStates: []
+		};
 	},
 	methods: {
 		onMouseMove: function(e: MouseEvent) {
@@ -39,9 +49,9 @@ export default Vue.extend({
 		onMouseDown: function() {
 			this.isMouseDown = true;
 		},
-
 		onMouseUp: function() {
 			this.isMouseDown = false;
+			this.saveCanvasState();
 		},
 		drawLine: function(x1: number, y1: number, x2: number, y2: number) {
 			let ctx = this.canvasContext;
@@ -54,10 +64,34 @@ export default Vue.extend({
 				ctx.stroke();
 				ctx.closePath();
 			}
+		},
+		saveCanvasState: function() {
+			const canvasRef = document.getElementById(this.id) as HTMLCanvasElement;
+			if (
+				canvasRef.toDataURL() !==
+				this.canvasStates[this.canvasStates.length - 1]
+			) {
+				this.canvasStates.push(canvasRef.toDataURL());
+			}
+		},
+		loadPreviousCanvasState: function() {
+			let ctx = this.canvasContext;
+			if (ctx) {
+				ctx.clearRect(0, 0, 300, 300);
+				// draw previous rect if the is one
+				if (this.canvasStates.length - 1 > 0) {
+					var canvasPic = new Image();
+					canvasPic.src = this.canvasStates[this.canvasStates.length - 2];
+					canvasPic.onload = function() {
+						ctx?.drawImage(canvasPic, 0, 0);
+					};
+				}
+				this.canvasStates.pop();
+			}
 		}
 	},
 	mounted() {
-		var canvasRef = document.getElementById(this.id) as HTMLCanvasElement;
+		const canvasRef = document.getElementById(this.id) as HTMLCanvasElement;
 		this.canvasContext = canvasRef.getContext("2d") as CanvasRenderingContext2D;
 	}
 });
