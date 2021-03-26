74306e0176f3a8a5607a9582625f796507955af2
HFourmy <nice_fourmy@hotmail.com>
2021-03-26 17:18:49 +0100

    Added routing

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,62 @@
 <template>
 	<div id="app">
-		<img alt="Vue logo" src="./assets/logo.png" />
-		<DrawingTraining />
+		<div id="nav">
+			<div class="rlink">
+				<router-link to="/">Home</router-link>
+			</div>
+			<div class="rlink">
+				<router-link class="router-link" to="/drawing">Drawing</router-link>
+			</div>
+		</div>
+		<div id="screens">
+			<router-view />
+		</div>
 	</div>
 </template>
 
-<script lang="ts">
-import Vue from "vue";
-import DrawingTraining from "@/screens/DrawingTraining.vue";
-
-export default Vue.extend({
-	name: "App",
-	components: {
-		DrawingTraining
-	}
-});
-</script>
-
 <style>
+html {
+	background-color: #16161d;
+	color: white;
+	height: 100%;
+	display: flex;
+	flex-grow: 1;
+	min-height: 600px;
+}
+body {
+	padding: 0px;
+	margin: 0px;
+	flex-grow: 1;
+}
 #app {
 	font-family: Avenir, Helvetica, Arial, sans-serif;
 	-webkit-font-smoothing: antialiased;
 	-moz-osx-font-smoothing: grayscale;
 	text-align: center;
 	color: #2c3e50;
-	margin-top: 60px;
+	display: flex;
+	flex-direction: row;
+	flex: 1;
+	height: 100%;
 }
-html {
-	background-color: #16161d;
-	color: white;
+
+#nav {
+	width: 100px;
+	box-shadow: 3px -5px 5px 0px black;
+}
+
+#screens {
+	flex: 1;
+}
+#nav a {
+	font-weight: bold;
+	color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+	color: #42b983;
+}
+.router-link {
+	flex: 0 0 100%;
 }
 </style>
---
+++ src/router/index.ts
@@ -0,0 +1,28 @@
+import Vue from "vue";
+import VueRouter, { RouteConfig } from "vue-router";
+import Home from "../screens/Home.vue";
+
+Vue.use(VueRouter);
+
+const routes: Array<RouteConfig> = [
+	{
+		path: "/",
+		name: "Home",
+		component: Home
+	},
+	{
+		path: "/drawing",
+		name: "Drawing",
+		// route level code-splitting
+		// this generates a separate chunk (about.[hash].js) for this route
+		// which is lazy-loaded when the route is visited.
+		component: () =>
+			import(/* webpackChunkName: "about" */ "../screens/DrawingTraining.vue")
+	}
+];
+
+const router = new VueRouter({
+	routes
+});
+
+export default router;
